7f9d03ea50d6f33a65e01992eb907744995fe75c
RaduP13 <stefanplotoaga@yahoo.ca>
2020-10-13 19:46:15 -0400

    Update Profile.java
    Added Javadoc documentation

--- backend/src/main/java/team23/smartHomeSimulator/model/Profile.java
+++ backend/src/main/java/team23/smartHomeSimulator/model/Profile.java
@@ -1,13 +1,44 @@
 package team23.smartHomeSimulator.model;
 
+/**
+ * This class is made for instantiating various profile within the smart home
+ * dashboard and handle operations related to dashboard functionalities
+ */
 public class Profile {
 
+    /**
+     * The name of the profile
+     */
     private String name;
+
+    /**
+     * The Location of the Profile in the house
+     */
     private String location;
+
+    /**
+     * The role of the Profile in the House
+     */
     private String role;
+
+    /**
+     * Permission level that he profile has access
+     */
     private String permission;
+
+    /**
+     * Current Status of the Profile, is it he current connected one or not
+     */
     private boolean isActive;
 
+    /**
+     * Parametrised Constructor
+     * @param Name name of the profile
+     * @param Location current location of the profile
+     * @param Role role attributed to the profile
+     * @param Permission permission level profile has
+     * @param IsActive current status of this profile
+     */
     public Profile(String Name,String Location,String Role, String Permission,boolean IsActive){
 
         this.name = Name;
@@ -16,6 +47,11 @@ public class Profile {
         this.permission = Permission;
         this.isActive = IsActive;
     }
+
+    /**
+     * Base Profile Constructor Initialising a Profile with only a name
+     * @param Name name of the base profile
+     */
     public Profile(String Name){
         this.name = Name;
         this.location = "Outside";
@@ -24,44 +60,84 @@ public class Profile {
         this.isActive = false;
     }
 
+    /**
+     *
+     * @return returns the name of the profile
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return returns the location of the profile
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     *
+     * @return the role of the profile
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     *
+     * @return the permission level of the profile
+     */
     public String getPermission() {
         return permission;
     }
 
+    /**
+     *
+     * @return the state of the profile
+     */
     public boolean getActive() {
         return isActive;
     }
 
+    /**
+     * sets the name of the profile
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * sets the location of the profile
+     * @param location
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * sets the role of the profile
+     * @param role
+     */
     public void setRole(String role) {
         this.role = role;
     }
 
+    /**
+     * sets the permission level of the profile
+     * @param permission
+     */
     public void setPermission(String permission) {
         this.permission = permission;
     }
 
+    /**
+     * sets the state of the profile in relation to the dashboard
+     * @param active
+     */
     public void setActive(boolean active) {
         isActive = active;
     }
-    
+
 }
