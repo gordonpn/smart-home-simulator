21705515003f5962d7b8805b969eff3e762fb32c
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-10-14 09:35:55 -0400

    ci: do not try to commit formatted files,
    display them on output and fail the ci if formatting is needed.

--- backend/src/main/java/team23/smartHomeSimulator/model/Profile.java
+++ backend/src/main/java/team23/smartHomeSimulator/model/Profile.java
@@ -1,159 +1,142 @@
 package team23.smartHomeSimulator.model;
-//import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 /**
- * This class is made for instantiating various profile within the smart home
- * dashboard and handle operations related to dashboard functionalities
+ * This class is made for instantiating various profile within the smart home dashboard and handle
+ * operations related to dashboard functionalities
  */
-//@Component
+// @Component
 public class Profile {
 
-    /**
-     * The name of the profile
-     */
-    private String name;
+  /** The name of the profile */
+  private String name;
 
-    /**
-     * The Location of the Profile in the house
-     */
-    private String location;
+  /** The Location of the Profile in the house */
+  private String location;
 
-    /**
-     * The role of the Profile in the House
-     */
-    private String role;
+  /** The role of the Profile in the House */
+  private String role;
 
-    /**
-     * Permission level that he profile has access
-     */
-    private String permission;
+  /** Permission level that he profile has access */
+  private String permission;
 
-    /**
-     * Current Status of the Profile, is it he current connected one or not
-     */
-    private boolean isActive;
+  /** Current Status of the Profile, is it he current connected one or not */
+  private boolean isActive;
 
-    /**
-     * Parametrised Constructor
-     * @param name name of the profile
-     * @param role role attributed to the profile
-     * @param permission permission level profile has
-     */
-    public Profile(String name,String location,String role, String permission){
+  /**
+   * Parametrised Constructor
+   *
+   * @param name name of the profile
+   * @param role role attributed to the profile
+   * @param permission permission level profile has
+   */
+  public Profile(String name, String location, String role, String permission) {
 
-        this.name = name;
-        this.location = location;
-        this.role = role;
-        this.permission = permission;
-        this.isActive = false;
-    }
+    this.name = name;
+    this.location = location;
+    this.role = role;
+    this.permission = permission;
+    this.isActive = false;
+  }
 
-    /**
-     * Base Profile Constructor Initialising a Profile with only a name
-     * @param name name of the base profile
-     */
-    public Profile(String name){
-        this.name = name;
-        this.location = "Outside";
-        this.role = "Guest";
-        this.permission = "none";
-        this.isActive = false;
-    }
+  /**
+   * Base Profile Constructor Initialising a Profile with only a name
+   *
+   * @param name name of the base profile
+   */
+  public Profile(String name) {
+    this.name = name;
+    this.location = "Outside";
+    this.role = "Guest";
+    this.permission = "none";
+    this.isActive = false;
+  }
 
-    /**
-     *
-     * @return returns the name of the profile
-     */
-    public String getName() {
-        return name;
-    }
+  /** @return returns the name of the profile */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     *
-     * @return returns the location of the profile
-     */
-    public String getLocation() {
-        return location;
-    }
+  /** @return returns the location of the profile */
+  public String getLocation() {
+    return location;
+  }
 
-    /**
-     *
-     * @return the role of the profile
-     */
-    public String getRole() {
-        return role;
-    }
+  /** @return the role of the profile */
+  public String getRole() {
+    return role;
+  }
 
-    /**
-     *
-     * @return the permission level of the profile
-     */
-    public String getPermission() {
-        return permission;
-    }
+  /** @return the permission level of the profile */
+  public String getPermission() {
+    return permission;
+  }
 
-    /**
-     *
-     * @return the state of the profile
-     */
-    public boolean getActive() {
-        return isActive;
-    }
+  /** @return the state of the profile */
+  public boolean getActive() {
+    return isActive;
+  }
 
-    /**
-     * sets the name of the profile
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * sets the name of the profile
+   *
+   * @param name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    /**
-     * sets the location of the profile
-     * @param location
-     */
-    public void setLocation(String location) {
-        this.location = location;
-    }
+  /**
+   * sets the location of the profile
+   *
+   * @param location
+   */
+  public void setLocation(String location) {
+    this.location = location;
+  }
 
-    /**
-     * sets the role of the profile
-     * @param role
-     */
-    public void setRole(String role) {
-        this.role = role;
-    }
+  /**
+   * sets the role of the profile
+   *
+   * @param role
+   */
+  public void setRole(String role) {
+    this.role = role;
+  }
 
-    /**
-     * sets the permission level of the profile
-     * @param permission
-     */
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
+  /**
+   * sets the permission level of the profile
+   *
+   * @param permission
+   */
+  public void setPermission(String permission) {
+    this.permission = permission;
+  }
 
-    /**
-     * sets the state of the profile in relation to the dashboard
-     * @param active
-     */
-    public void setActive(boolean active) {
-        isActive = active;
-    }
+  /**
+   * sets the state of the profile in relation to the dashboard
+   *
+   * @param active
+   */
+  public void setActive(boolean active) {
+    isActive = active;
+  }
 
-    /**
-     * Method to set all the attributes of a profile
-     * @param name new name of profile
-     * @param location new location of profile
-     * @param role new role of profile
-     * @param permission new permission of profile
-     * @param isActive new active status of profile
-     */
-    public void setAll (String name,String location,String role, String permission,boolean isActive){
-        this.name = name;
-        this.location = location;
-        this.role = role;
-        this.permission = permission;
-        this.isActive = isActive;
-    }
-
+  /**
+   * Method to set all the attributes of a profile
+   *
+   * @param name new name of profile
+   * @param location new location of profile
+   * @param role new role of profile
+   * @param permission new permission of profile
+   * @param isActive new active status of profile
+   */
+  public void setAll(
+      String name, String location, String role, String permission, boolean isActive) {
+    this.name = name;
+    this.location = location;
+    this.role = role;
+    this.permission = permission;
+    this.isActive = isActive;
+  }
 }
