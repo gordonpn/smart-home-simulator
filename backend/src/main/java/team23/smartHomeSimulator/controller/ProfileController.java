eb1dd73b435a3f265a851e6a0e54f2fe9b8f5b66
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-10-15 11:41:00 -0400

    style: fix import order

--- backend/src/main/java/team23/smartHomeSimulator/controller/ProfileController.java
+++ backend/src/main/java/team23/smartHomeSimulator/controller/ProfileController.java
@@ -2,7 +2,6 @@ package team23.smartHomeSimulator.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
