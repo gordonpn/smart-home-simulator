f54bf70e329592cc1ff62c50e8fe1ed0327b3c35
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-10-19 18:27:28 -0400

    tests: add missing test for unblock/block controller

--- backend/src/test/java/team23/smartHomeSimulator/BlockWindowsTest.java
+++ backend/src/test/java/team23/smartHomeSimulator/BlockWindowsTest.java
@@ -59,4 +59,41 @@ public class BlockWindowsTest {
                     containsString(
                         "{\"window-1\":{\"isOpen\":false,\"isBlocked\":false},\"window-2\":{\"isOpen\":false,\"isBlocked\":false}}")));
   }
+
+  @Test
+  public void shouldBlockAndUnblockWindow() throws Exception {
+    MockHttpServletRequestBuilder builderBlock =
+        MockMvcRequestBuilders.put("/api/rooms/windows/block-window")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding("UTF-8")
+            .content("{\"windowName\":\"window-1\",\"roomName\":\"bedroom3\"}");
+
+    this.mockMvc
+        .perform(builderBlock)
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .string(
+                    containsString(
+                        "{\"window-1\":{\"isOpen\":false,\"isBlocked\":true},\"window-2\":{\"isOpen\":false,\"isBlocked\":false}}")));
+
+    MockHttpServletRequestBuilder builderUnblock =
+        MockMvcRequestBuilders.delete("/api/rooms/windows/block-window")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding("UTF-8")
+            .content("{\"windowName\":\"window-1\",\"roomName\":\"bedroom3\"}");
+
+    this.mockMvc
+        .perform(builderUnblock)
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .string(
+                    containsString(
+                        "{\"window-1\":{\"isOpen\":false,\"isBlocked\":false},\"window-2\":{\"isOpen\":false,\"isBlocked\":false}}")));
+  }
 }
