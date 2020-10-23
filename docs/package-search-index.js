b894e1ee5d3f96b1aeb88dcf1cbab2d9d424fe20
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-10-23 11:38:03 -0400

    docs: add generated javadoc to host with github pages

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"team23.smartHomeSimulator"},{"l":"team23.smartHomeSimulator.controller"},{"l":"team23.smartHomeSimulator.model"},{"l":"team23.smartHomeSimulator.model.request_body"}]
