26d956e36e81dec428157ecabfc53a11121af24e
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-11-05 22:21:00 -0500

    feat: integrate save and load with frontend
    refs:
    issue #56

--- client/src/components/Modules/SHSTab.js
+++ client/src/components/Modules/SHSTab.js
@@ -4,6 +4,8 @@ import AddUser from "./AddUser";
 import Box from "@material-ui/core/Box";
 import RemoveUser from "./RemoveUser";
 import EditUser from "./EditUser";
+import LoadProfiles from "@/src/components/Modules/LoadProfiles";
+import SaveProfiles from "@/src/components/Modules/SaveProfiles";
 
 export default function SHSTab() {
   return (
@@ -20,6 +22,14 @@ export default function SHSTab() {
         </Box>
         <Box p={0.5}>
           <EditUser />
+        </Box>
+      </Box>
+      <Box display="flex" flexDirection="row">
+        <Box p={0.5}>
+          <SaveProfiles />
+        </Box>
+        <Box p={0.5}>
+          <LoadProfiles />
         </Box>
       </Box>
     </>
