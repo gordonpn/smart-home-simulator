2d9629282a755df9ca1d4cb81dd6d6eb86e8fa0a
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-11-08 16:48:12 -0500

    feat: add logs to automatica away mode

--- client/src/components/Sidebar/AwayMode.js
+++ client/src/components/Sidebar/AwayMode.js
@@ -2,26 +2,38 @@ import RunningStateStore from "@/src/stores/RunningStateStore";
 import { Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import ProfileStore from "@/src/stores/ProfileStore";
+import ConsoleStore from "@/src/stores/ConsoleStore";
 
 export default function AwayMode() {
   const { currentState } = RunningStateStore();
   const { profiles } = ProfileStore();
   const [awayMode, setAwayMode] = useState(false);
+  const { appendToLogs } = ConsoleStore();
 
   useEffect(() => {
     const fetchAwayMode = async () => {
       if (currentState) {
         for (const profile of profiles) {
           if (profile.location.toLowerCase() !== "outside") {
+            appendToLogs({
+              timestamp: new Date(),
+              message: "Away mode has been turned off automatically",
+              module: "SHP",
+            });
             setAwayMode(false);
             return;
           }
         }
+        appendToLogs({
+          timestamp: new Date(),
+          message: "Away mode has been turned on automatically",
+          module: "SHP",
+        });
         setAwayMode(true);
       }
     };
     fetchAwayMode();
-  }, [currentState, profiles]);
+  }, [appendToLogs, currentState, profiles]);
 
   return (
     <>
