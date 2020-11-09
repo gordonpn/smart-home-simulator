3c05ab98e75a210d45d150d0e98e63765df341bd
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-11-09 13:57:07 -0500

    feat: away mode is now a button manually clicked

--- client/src/components/Modules/AwayMode.js
+++ client/src/components/Modules/AwayMode.js
@@ -1,51 +1,120 @@
 import RunningStateStore from "@/src/stores/RunningStateStore";
-import { Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import ProfileStore from "@/src/stores/ProfileStore";
 import ConsoleStore from "@/src/stores/ConsoleStore";
+import Button from "@material-ui/core/Button";
+import HouseStore from "@/src/stores/HouseStore";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import Typography from "@material-ui/core/Typography";
+import formStyles from "@/src/styles/formStyles";
 
 export default function AwayMode() {
-  const { currentState } = RunningStateStore();
-  const { profiles } = ProfileStore();
-  const [awayMode, setAwayMode] = useState(false);
+  const [everyoneOutside, setEveryoneOutside] = useState(false);
+  const [openError, setOpenError] = useState(false);
+  const [openPermissionError, setOpenPermissionError] = useState(false);
+  const classes = formStyles();
   const { appendToLogs } = ConsoleStore();
+  const { awayMode, setAwayMode } = HouseStore();
+  const { currentState } = RunningStateStore();
+  const { profiles, currentProfile } = ProfileStore();
 
   useEffect(() => {
-    const fetchAwayMode = async () => {
-      if (currentState) {
-        for (const profile of profiles) {
-          if (profile.location.toLowerCase() !== "outside") {
-            if (awayMode) {
-              appendToLogs({
-                timestamp: new Date(),
-                message: "Away mode has been turned off automatically",
-                module: "SHP",
-              });
-            }
-            setAwayMode(false);
-            return;
-          }
+    const isEveryoneOutside = () => {
+      for (const profile of profiles) {
+        if (profile.location.toLowerCase() !== "outside") {
+          setEveryoneOutside(false);
+          return;
         }
-        if (!awayMode) {
-          appendToLogs({
-            timestamp: new Date(),
-            message: "Away mode has been turned on automatically",
-            module: "SHP",
-          });
-        }
-        setAwayMode(true);
       }
+      setEveryoneOutside(true);
     };
-    fetchAwayMode();
-  }, [appendToLogs, awayMode, currentState, profiles]);
+    isEveryoneOutside();
+  }, [profiles]);
+
+  const handleClick = () => {
+    if (
+      currentProfile.permission.toLowerCase() === "stranger" ||
+      currentProfile.permission.toLowerCase() === "guest"
+    ) {
+      setOpenPermissionError(true);
+      return;
+    }
+    if (!everyoneOutside && !awayMode) {
+      setOpenError(true);
+      return;
+    }
+    setAwayMode(!awayMode);
+    if (awayMode) {
+      appendToLogs({
+        timestamp: new Date(),
+        message: "Away mode has been turned on automatically",
+        module: "SHP",
+      });
+    } else {
+      appendToLogs({
+        timestamp: new Date(),
+        message: "Away mode has been turned off automatically",
+        module: "SHP",
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setOpenError(false);
+    setOpenPermissionError(false);
+  };
 
   return (
     <>
       {currentState && (
-        <Typography variant="h6" color="primary">
+        <Button
+          variant="outlined"
+          color="primary"
+          size="large"
+          onClick={handleClick}
+        >
           Away Mode: {awayMode ? "ON" : "OFF"}
-        </Typography>
+        </Button>
       )}
+      <Modal
+        className={classes.modal}
+        open={openError}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openError}>
+          <div className={classes.paper}>
+            <Typography variant="h6">
+              Cannot set away mode to ON when there are still people in the
+              house
+            </Typography>
+          </div>
+        </Fade>
+      </Modal>
+      <Modal
+        className={classes.modal}
+        open={openPermissionError}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openPermissionError}>
+          <div className={classes.paper}>
+            <Typography variant="h6">
+              Your permission does not allow changing this setting
+            </Typography>
+          </div>
+        </Fade>
+      </Modal>
     </>
   );
 }
